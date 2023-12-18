9eb27422a379201b34af9a078297d91db7ba95bd
Admin Stagiaire <yannickpitaud@gmail.com>
2023-12-18 17:17:56 +0100

    Initialisation

---
+++ src/main/java/com/example/Beatgenius/BeatGeniusApplication.java
@@ -0,0 +1,13 @@
+package com.example.Beatgenius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatGeniusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatGeniusApplication.class, args);
+	}
+
+}
